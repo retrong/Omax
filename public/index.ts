a21ee577ab10d59eaa74b6d32f858298365bb248
retrong <eguobabarth@gmail.com>
2023-11-06 18:27:21 +0100

    created accordion component

--- public/index.ts
+++ public/index.ts
@@ -2,11 +2,14 @@ import arrow from './arrow.png';
 import close from './close.svg';
 import double from './double-arrow.png';
 import dub from './double.gif';
+import down from './down.svg';
 import second from './img2.png';
 import blur from './img5.png';
 import main from './main.png';
 import test from './not found imag.png';
 import open from './open.svg';
+import up from './up.svg';
 
-export { arrow, blur, close, double, dub, main, open, second, test };
+
+export { arrow, blur, close, double, down, dub, main, open, second, test, up };
 
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { products } from '@/constants'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { down, up } from '../../../public'
+import './AccordionStyles.css'
+
+export default function Accordion() {
+
+    const [selected, setSelected] = useState(null)
+
+    const toggle = (i : any ) =>{
+        if (selected === i ) {
+            return setSelected(null)
+        }
+
+        setSelected(i)
+    }
+  return (
+    <div className='accordion'>
+        <div className='wrapper'>
+            {products.map((product, i) => (
+                <div key={product.id} className='item'>
+                    <div className='title' onClick={() => toggle(i)}>
+                        <h2>{product.title}</h2>
+                        <Image src={selected === i ? down : up} alt='navigation' />
+                        {/* <span>{selected === i ? down : up }</span> */}
+                    </div>
+                    <div className={`${selected === i ? 'content-show' : 'content'} content-main`}>
+                        {product.content}
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
